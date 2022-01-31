a58b2e4ba8fe7415174ea53e3d90c8ff92d9e6ab
Evgeny Samoilov <z.s.dp@outlook.com>
2022-01-31 09:13:03 +0200

    Сreated a project. Создал проект.

---
+++ CRM_t1.CMD/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CRM_t1.CMD
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Приложение запущено");
+        }
+    }
+}
