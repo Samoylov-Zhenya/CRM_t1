60071220e6601540e54a7c5ba1c364bf09389821
Evgeny Samoilov <z.s.dp@outlook.com>
2022-02-01 06:44:20 +0200

    Подготовил меню для удобной работы с UI консоли

--- CRM_t1.CMD/Program.cs
+++ CRM_t1.CMD/Program.cs
@@ -7,8 +7,40 @@ namespace CRM_t1.CMD
         static void Main(string[] args)
         {
             Console.WriteLine("Приложение запущено");
-            
-            Console.ReadLine();
+            MenuF();
+        }
+        static void MenuF()
+        {
+            for (; ; )
+            {
+                Console.WriteLine("Выберите действие (-h помощь)");
+                var str = Console.ReadLine();
+                switch (str)
+                {
+                    case "-n":
+                        NewUsee();
+                        break;
+                    case "-h":
+                        Hellp();
+                        break;
+                    case "-e":
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        static void NewUsee()
+        {
+
+        }
+        static void Hellp()
+        {
+            Console.WriteLine(
+                            $"-n новый пользователь\n" +
+                            $"-h помощь\n" +
+                            $"-e выход\n");
         }
     }
 }
