5f3a88ac529c4c7920b502d196b2e4c99c5d772b
Evgeny Samoilov <z.s.dp@outlook.com>
2022-02-01 12:41:33 +0200

    Warehouse History

---
+++ CRM_t1.BL/Model/WarehouseHistory.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRM_t1.BL.Model
+{
+    internal class WarehouseHistory
+    {
+        static int count = 0;
+        int ID;
+        int IdUser;
+        int IdProduct;
+        int priceProduct;
+        int quantityProduct;
+
+        public WarehouseHistory()
+        {
+            ID = count;
+            count++;
+        }
+        public override string ToString()
+        {
+            return
+               $"\nId\t{ID} " +
+               $"\nUser\t{IdUser} " +
+               $"\nProd.\t{IdProduct} " +
+               $"\nQuan.\t{quantityProduct} " +
+               $"\nPrice\t{priceProduct}" +
+               $"\nType\t{base.ToString()}";
+        }
+    }
+}
